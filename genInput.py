fc3ba314ad0515c1072ae9aca480d7b0c556e8f0
Fynn <enriquefynn@gmail.com>
2014-07-19 15:09:28 -0300

    better graph

--- genInput.py
+++ genInput.py
@@ -4,45 +4,56 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.Graph()
-n = 20
+ctbcClientsN = 100
+nonCtbcClientsN = 50
 f = open('input.txt', 'w')
 
-ctbcOrigN = 100
+ctbcN = 100
 nonCtbcN = 100
 ncont = 10
 callsPerN = 50
+callCtbcPercentage = 80
 ctbcNumbers = []
 nonCtbcNumbers = []
 
 for i in range(nonCtbcN):
     nonCtbcNumbers.append(rr(10000000, 90000000))
 
-for i in range(ctbcOrigN):
+for i in range(ctbcN):
     ctbcNumbers.append(rr(90000000,100000000))
 
-for i in range(n):
-    print(i)
-    ctbcNumber = ctbcNumbers[rr(0, ctbcOrigN)]
+for i in range(ctbcClientsN):
+    ctbcNumber = ctbcNumbers[rr(0, ctbcN)]
     G.add_node(ctbcNumber)
     ncontacts = rr(1,ncont)
     contacts = []
     
     for j in range(ncontacts):
-        contacts.append(nonCtbcNumbers[rr(0, nonCtbcN)])
-    contN = rr(1, ncont)
+        if (rr(0, 101) < callCtbcPercentage):
+            contacts.append(ctbcNumbers[rr(0, ctbcN)])
+        else:
+            contacts.append(nonCtbcNumbers[rr(0, nonCtbcN)])
 
-    for j in range(ncontacts):
-        c = nonCtbcNumbers[rr(0, ctbcOrigN)]
-        contacts.append(c)
-    
     ncalls = rr(1,callsPerN)
     for j in range(ncalls):
         dur = rr(1, 3600)
-        dst = rr(0, len(contacts))
-        data = '{} {} {} \n'.format(ctbcNumber, contacts[dst], dur)
-        G.add_node(contacts[dst])
+        dst = rr(0, ncontacts)
+        data = '{} {} {}\n'.format(ctbcNumber, contacts[dst], dur)
+        if (contacts[dst]//10000000 == 9):
+            G.add_node(contacts[dst], color='red')
+        else:
+            G.add_node(contacts[dst], color='blue')
         G.add_edge(ctbcNumber, contacts[dst])
         f.write(data)
 
-nx.draw(G, nx.spring_layout(G,iterations=100), node_color='b', node_size=20, with_labels=False)
+for i in range(nonCtbcClientsN):
+    nonCtbcNumber = nonCtbcNumbers[rr(0, nonCtbcN)]
+    G.add_node(nonCtbcNumber, color='red')
+    ctbcContact = ctbcNumbers[rr(0, ctbcN)]
+    for j in range(ncalls):
+        data = '{} {} {}\n'.format(nonCtbcNumber, ctbcContact, rr(1, 300))
+        G.add_node(ctbcContact, color='blue')
+        G.add_edge(nonCtbcNumber, ctbcContact)
+
+nx.draw(G, nx.spring_layout(G,iterations=100), node_size=80, with_labels=False)
 plt.show()
